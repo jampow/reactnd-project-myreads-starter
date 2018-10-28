cbd33abd3b35d79bc363e97476555883205e1f72
Gianpaulo M. Soares <jam_pow@hotmail.com>
2018-10-27 21:17:07 -0300

    feat(search): Apply debounce on search
    Add debounce on search field to reduce requests to the API when typing on input

--- src/views/Search.js
+++ src/views/Search.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import DebounceInput from 'react-debounce-input';
 import Book from '../components/Book';
 import { search } from '../BooksAPI';
 
@@ -10,7 +11,7 @@ class Search extends React.Component {
   }
 
   onChange = async e => {
-    const { value } = e.currentTarget;
+    const { value } = e.target;
 
     if (value === '') { return; }
 
@@ -41,7 +42,12 @@ class Search extends React.Component {
         <div className="search-books-bar">
           <Link to='/' className='close-search' />
           <div className="search-books-input-wrapper">
-            <input onChange={this.onChange} type="text" placeholder="Search by title or author"/>
+            <DebounceInput
+              minLength={3}
+              debounceTimeout={500}
+              onChange={this.onChange}
+              type="text"
+              placeholder="Search by title or author"/>
           </div>
         </div>
         <div className="search-books-results">
